441e9cb6a701bf3e5dcc1bb2829c04dbf200a28e
gorgick <gorgick2014@yandex.by>
2025-08-15 20:19:13 +0300

    view, url

--- translator/translator/urls.py
+++ translator/translator/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from words.views import WordViewSet
+
+router = SimpleRouter()
+router.register(r'word', WordViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
--- translator/words/views.py
+++ translator/words/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from words.models import Word
+from words.serializers import WordSerializer
+
+
+class WordViewSet(ModelViewSet):
+    queryset = Word.objects.all()
+    serializer_class = WordSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['name']
+
